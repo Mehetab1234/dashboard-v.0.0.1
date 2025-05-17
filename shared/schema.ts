66612517ccc72ab727afbca7ecc4d70749d02d22
Craze-Labes <mehetab277@gmail.com>
2025-05-17 21:27:11 +0530

    Update schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -1,4 +1,4 @@
-mport { pgTable, text, serial, integer, boolean, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, text, serial, integer, boolean, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
